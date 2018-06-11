a3b17ad36643504778eaf667d2a5a2af6029a748
ryukam10k <r.kamioka@gmail.com>
2018-06-12 06:38:03 +0900

    Validator::extendを使うのもあり

--- app/Providers/HelloServiceProvider.php
+++ app/Providers/HelloServiceProvider.php
@@ -20,6 +20,11 @@ class HelloServiceProvider extends ServiceProvider
         $validator->resolver(function($translator, $data, $rules, $messages) {
             return new HelloValidator($translator, $data, $rules, $messages);
         });
+        /* １つのコントローラでしか使わない検証の場合、こういう書き方もあり（手軽）
+        Validator::extend('hello', function($attribute, $value, $parameters, $validator) {
+            return $value % 2 == 0;
+        });
+        */
     }
 
     /**
