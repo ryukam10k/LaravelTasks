e0276096b2110b27bb2e25e79c04525bc1b63eb9
ryukam10k <r.kamioka@gmail.com>
2018-06-19 06:36:30 +0900

    DBクラスを使ってselect文を発行

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,19 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 use App\Http\Requests\HelloRequest;
 use Validator;
 
 class HelloController extends Controller
 {
     public function index(Request $request) {
-        if ($request->hasCookie('msg'))
-        {
-            $msg = 'Cookie: ' . $request->cookie('msg');
-        } else {
-            $msg = '※クッキーはありません。';
-        }
-        return view('hello.index', ['msg'=> $msg ]);
+        $items = DB::select('select * from people');
+        return view('hello.index', ['items'=> $items ]);
     }
 
     public function post(Request $request) {
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -8,28 +8,15 @@
 @endsection
 
 @section('content')
-    <p>{{$msg}}</p>
-    @if (count($errors) > 0)
-    <p>入力に問題があります。再入力してください。</p>
-    @endif
     <table>
-        <form action="/hello" method="post">
-            {{ csrf_field() }}
-            @if ($errors->has('msg'))
+        <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
+        @foreach ($items as $item)
             <tr>
-                <th>ERROR</th>
-                <td>{{$errors->first('msg')}}</td>
-            <tr>
-            @endif
-            <tr>
-                <th>Message: </th>
-                <td><input type="text" name="msg" value="{{old('msg')}}"></td>
+                <td>{{$item->name}}</td>
+                <td>{{$item->mail}}</td>
+                <td>{{$item->age}}</td>
             </tr>
-            <tr>
-                <th></th>
-                <td><input type="submit" value="send"></td>
-            </tr>
-        </form>
+        @endforeach
     </table>
 @endsection
 
