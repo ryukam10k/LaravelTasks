30bc20e56ab1886167a031ac77f76a0619b4ced1
ryukam10k <r.kamioka@gmail.com>
2018-07-09 17:34:13 +0900

    delete文

--- routes/web.php
+++ routes/web.php
@@ -48,4 +48,7 @@ Route::post('hello/add', 'HelloController@create');
 Route::get('hello/edit', 'HelloController@edit');
 Route::post('hello/edit', 'HelloController@update');
 
+Route::get('hello/del', 'HelloController@del');
+Route::post('hello/del', 'HelloController@remove');
+
 Route::get('single', 'SingleHelloController');
