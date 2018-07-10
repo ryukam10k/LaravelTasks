f8cd9765d98de63ea0706c7431678cde546a6f12
ryukam10k <r.kamioka@gmail.com>
2018-07-10 14:33:55 +0900

    use update queryBuilder

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -48,9 +48,12 @@ class HelloController extends Controller
 
     public function edit(Request $request)
     {
-        $param = ['id' => $request->id];
-        $item = DB::select('select * from people where id = :id', $param);
-        return view('hello.edit', ['form' => $item[0]]);
+        //$param = ['id' => $request->id];
+        //$item = DB::select('select * from people where id = :id', $param);
+        $item = DB::table('people')
+            ->where('id', $request->id)
+            ->first();
+        return view('hello.edit', ['form' => $item]);
     }
 
     public function update(Request $request)
@@ -61,7 +64,10 @@ class HelloController extends Controller
             'mail' => $request->mail,
             'age' => $request->age,
         ];
-        DB::update('update people set name=:name, mail=:mail, age=:age where id=:id', $param);
+        //DB::update('update people set name=:name, mail=:mail, age=:age where id=:id', $param);
+        DB::table('people')
+            ->where('id', $request->id)
+            ->update($param);
         return redirect('/hello');
     }
 
