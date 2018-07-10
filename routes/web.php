b70225e59dd503e1797b0413fc33f82f271b92f7
ryukam10k <r.kamioka@gmail.com>
2018-07-10 12:35:37 +0900

    add show.php

--- routes/web.php
+++ routes/web.php
@@ -51,4 +51,6 @@ Route::post('hello/edit', 'HelloController@update');
 Route::get('hello/del', 'HelloController@del');
 Route::post('hello/del', 'HelloController@remove');
 
+Route::get('hello/show', 'HelloController@show');
+
 Route::get('single', 'SingleHelloController');
