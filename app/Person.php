0ced4bc27c2cf3f9bbe5a5214a5a79296b4c95bd
ryukam10k <r.kamioka@gmail.com>
2018-07-11 15:44:06 +0900

    use global scope

--- app/Person.php
+++ app/Person.php
@@ -3,9 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Person extends Model
 {
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('age', function(builder $builder){
+            $builder->where('age', '>', 20);
+        });
+    }
+
     public function getData()
     {
         return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
@@ -25,4 +35,5 @@ class Person extends Model
     {
         return $query->where('age', '<=', $n);
     }
+
 }
