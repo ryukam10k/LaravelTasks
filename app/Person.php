c68adb5024b7bf13aab68a5e6261d555aad6bf30
ryukam10k <r.kamioka@gmail.com>
2018-07-12 17:25:28 +0900

    add crud processing using model

--- app/Person.php
+++ app/Person.php
@@ -19,19 +19,11 @@ class Person extends Model
         return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
     }
 
-    public function scopeNameEqual($query, $str)
-    {
-        return $query->where('name', $str);
-    }
+    protected $guarded = array('id');
 
-    public function scopeAgeGreaterThan($query, $n)
-    {
-        return $query->where('age', '>=', $n);
-    }
-
-    public function scopeAgeLessThan($query, $n)
-    {
-        return $query->where('age', '<=', $n);
-    }
-
+    public static $rules = array(
+        'name' => 'required',
+        'mail' => 'email',
+        'age' => 'integer|min:0|max:150'
+    );
 }
--- resources/views/person/index.blade.php
+++ resources/views/person/index.blade.php
@@ -8,13 +8,19 @@
 @endsection
 
 @section('content')
+    <div><a href="/person/add">Add</a></div>
     <table>
         <tr>
             <th>Data</th>
+            <th></th>
         </tr>
         @foreach ($items as $item)
         <tr>
             <td>{{$item->getData()}}</td>
+            <td>
+                <a href="/person/edit?id={{$item->id}}">edit | </a>
+                <a href="/person/del?id={{$item->id}}">del</a>
+            </td>
         </tr>
         @endforeach
     </table>
