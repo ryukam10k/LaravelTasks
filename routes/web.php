455cb20e31f485487f3c5aebce779ef029d51250
ryukam10k <r.kamioka@gmail.com>
2018-07-13 13:27:20 +0900

    add rest classes

--- routes/web.php
+++ routes/web.php
@@ -81,3 +81,6 @@ Route::get('board', 'BoardController@index');
 
 Route::get('board/add', 'BoardController@add');
 Route::post('board/add', 'BoardController@create');
+
+/* Rest */
+Route::resource('rest', 'RestappController');
