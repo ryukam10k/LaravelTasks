e71850e9215e4e6a23a771b4cb4c0bc6162173bf
ryukam10k <r.kamioka@gmail.com>
2018-07-13 10:19:43 +0900

    use has & doesntHave method

--- resources/views/person/index.blade.php
+++ resources/views/person/index.blade.php
@@ -15,23 +15,32 @@
             <th>Board</th>
             <th></th>
         </tr>
-        @foreach ($items as $item)
+        @foreach ($hasItems as $item)
+            <tr>
+                <td>{{$item->getData()}}</td>
+                <td>
+                    <table widht="100%">
+                    @foreach ($item->boards as $obj)
+                        <tr><td>{{$obj->getData()}}</td></tr>
+                    @endforeach
+                    </table>
+                </td>
+                <td>
+                    <a href="/person/edit">edit | </a>
+                    <a href="/person/del">del</a>
+                </td>
+            </tr>
+        @endforeach
+    </table>
+    <div style="margin:10px;"></div>
+    <table>
         <tr>
-            <td>{{$item->getData()}}</td>
-            <td>
-            @if ($item->boards != null)
-                <table widht="100%">
-                @foreach ($item->boards as $obj)
-                    <tr><td>{{$obj->getData()}}</td></tr>
-                @endforeach
-                </table>
-            @endif
-            </td>
-            <td>
-                <a href="/person/edit?id={{$item->id}}">edit | </a>
-                <a href="/person/del?id={{$item->id}}">del</a>
-            </td>
+            <th>Person</th>
         </tr>
+        @foreach ($noItems as $item)
+            <tr>
+                <td>{{$item->getData()}}</td>
+            </tr>
         @endforeach
     </table>
     <div><a href="/board">go to board</a></div>
